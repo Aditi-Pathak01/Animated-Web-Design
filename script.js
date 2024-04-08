0189b56cd258431b62354fcb340f3ec6e0a0d401
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-04-08 22:06:07 +0530

    locomotiveJS added

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  });
+
+  // --- PURPLE/GREEN PANEL ---
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
 function navAnimation() {
   let nav = document.querySelector("nav");
   nav.addEventListener("mouseenter", () => {
@@ -105,8 +143,23 @@ const page5Animation = () => {
     page5Right.childNodes[3].style.opacity = 0;
     page5Right.childNodes[3].load();
   });
+  page5Right.addEventListener("mousemove", (dets) => {
+    gsap.to(page5Right.childNodes[5], {
+      opacity: 1,
+      scale: 1.5,
+      x: dets.x - page5Right.getBoundingClientRect().x,
+      y: dets.y - page5Right.getBoundingClientRect().y,
+    });
+  });
+  page5Right.addEventListener("mouseleave", () => {
+    gsap.to(page5Right.childNodes[5], {
+      opacity: 0,
+      scale: 0,
+    });
+  });
 };
 
+locomotiveAnimation();
 navAnimation();
 page2Animation();
 page3Animation();
